a390904a174dcf6bafe2032102fb43aa81100fe3
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-23 02:45:20 -0500

    5th commit: populated my Crust enums with names.

--- src/main/java/com/pluralsight/enums/CrustType.java
+++ src/main/java/com/pluralsight/enums/CrustType.java
@@ -1,4 +1,26 @@
 package com.pluralsight.enums;
 
 public enum CrustType {
+
+    THIN("Thin");
+    REGULAR("Regula");
+    THICK("Thick");
+    CAULIFLOWER("Cauliflower");
+
+    private final String name;
+
+    CrustType(String name){
+
+        this.name = name;
+    }
+    public String getName() {
+
+        return name;
+    }
+
+    @Override
+    public String toString() {
+
+        return name;
+    }
 }
