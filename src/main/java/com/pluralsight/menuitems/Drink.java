f92b73359c7dea4beef0e4710e4478b740049871
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-24 00:06:41 -0500

    10th commit: updated my drink class to extend OrderItem and to implement calculatePrice method. also added a constructor and a flavor variable;

--- src/main/java/com/pluralsight/menuitems/Drink.java
+++ src/main/java/com/pluralsight/menuitems/Drink.java
@@ -1,4 +1,26 @@
 package com.pluralsight.menuitems;
 
-public class Drink {
+import com.pluralsight.abstracts.OrderItem;
+import com.pluralsight.enums.DrinkSize;
+
+public class Drink extends OrderItem {
+
+    private DrinkSize size;
+    private String flavor;
+
+    public Drink(DrinkSize size, String flavor) {
+
+        super(flavor, size.getPrice());
+
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double calculatePrice(){
+
+        return size.getPrice();
+    }
+
+    //@Override
 }
