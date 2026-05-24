e3ec6f3783cc894f247236a6abd9fc01d4659f77
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-24 04:30:16 -0500

    14th commit: updated my Toppings class to include getExtraPrice method and added a comment to clarify why what this method is for so that I won't confuse it with the getPrice method that is above it.

--- src/main/java/com/pluralsight/menuitems/Topping.java
+++ src/main/java/com/pluralsight/menuitems/Topping.java
@@ -14,21 +14,43 @@ public class Topping {
         this.type = type;
     }
 
-    //this is to get the price of this
+    //this is to get the price of this topping based on size and type
     public double getPrice (PizzaSize size) {
 
         switch (type){
 
-            case MEAT:
+            case MEAT, CHEESE:
+
                 return getPrice(size);
-            case CHEESE:
-                return getPrice(size);
+
             case REGULAR:
 
                 //hard-coded zero because regular toppings are free.
                 return 0.00;
             default:
                 return 0.00;
+        }
+
+    }
+
+    //this is to get the extra price of this topping based on size and type
+    public double getExtraPrice(PizzaSize size){
+
+        switch (type){
+
+            case MEAT, CHEESE:
+
+                return getExtraPrice(size);
+
+            case REGULAR:
+
+                //hard-coded zero because regular toppings are free even if they are extra toppings
+                return 0.00;
+
+            default:
+
+                return 0.00;
+
         }
 
     }
