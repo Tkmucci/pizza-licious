f517b6228e4fc77da54cb9117f1d933a19923161
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-23 23:56:51 -0500

    8th commit: populated my SauceType enum with the sauceNames, added a toString method that returns the sauceName.

--- src/main/java/com/pluralsight/enums/SauceType.java
+++ src/main/java/com/pluralsight/enums/SauceType.java
@@ -1,4 +1,28 @@
 package com.pluralsight.enums;
 
 public enum SauceType {
+
+    MARINARA("Marinara"),
+    ALFREDO("Alfredo"),
+    PESTO("Pesto"),
+    BBQ("BBQ"),
+    BUFFALO("Buffalo"),
+    OLIVE_OIL("Olive Oil");
+
+    private String sauceName;
+
+    SauceType(String sauceName) {
+        this.sauceName = sauceName;
+    }
+
+    public String getSauceName() {
+        return sauceName;
+    }
+
+    @Override
+    public String toString() {
+
+        return sauceName;
+    }
+
 }
