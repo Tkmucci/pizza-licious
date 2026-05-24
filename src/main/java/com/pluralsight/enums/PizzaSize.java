4e852bf0afd490314cdbf3b2356b194a3e8568e3
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-23 23:51:46 -0500

    7th commit: populated my PizzaSize enum with the pizza sizes and display names, added a toString method that returns the display name.

--- src/main/java/com/pluralsight/enums/PizzaSize.java
+++ src/main/java/com/pluralsight/enums/PizzaSize.java
@@ -1,4 +1,37 @@
 package com.pluralsight.enums;
 
 public enum PizzaSize {
+
+    SMALL_8(8.50,"8\"","Small"),
+    MEDIUM_12(12.00,"12\"","Medium"),
+    LARGE_16(16.50,"16\"","Large");
+
+    private final double basePrice;
+    private final String display;
+    private final String name;
+
+    PizzaSize(double basePrice, String display, String name) {
+
+        this.display = display;
+        this.basePrice = basePrice;
+        this.name = name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+
+        return display;
+    }
 }
