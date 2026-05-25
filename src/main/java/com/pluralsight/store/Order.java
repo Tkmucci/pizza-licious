8671dd011ba2f947de32bfc9f8e8092c5c7b90c9
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-25 11:04:54 -0500

    21st commit: updated my Order class to have my variables that I think will be needed and a constructor.

--- src/main/java/com/pluralsight/store/Order.java
+++ src/main/java/com/pluralsight/store/Order.java
@@ -3,12 +3,21 @@ package com.pluralsight.store;
 import com.pluralsight.abstracts.OrderItem;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
 
-    private List<OrderItem> orderItems;
+    private List<OrderItem> items;
     private String customerName;
     private LocalDateTime orderTime;
+
+    //for testing
+    public Order(){
+
+        this.items = new ArrayList<>();
+        this.orderTime = LocalDateTime.now();
+        this.customerName = "Guest";
+    }
 
 }
