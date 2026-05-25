8d393ee0ff3f152b3ebeb47c7ab5461de11c084b
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-25 16:39:53 -0500

    39th commit: I updated my ReceiptManager class to include a constructor that creates a folder if it doesn't already exist.

--- src/main/java/com/pluralsight/store/ReceiptManager.java
+++ src/main/java/com/pluralsight/store/ReceiptManager.java
@@ -1,4 +1,20 @@
 package com.pluralsight.store;
 
+import java.io.File;
+
 public class ReceiptManager {
+
+    private static final String RECEIPTS_FOLDER = "receipts";
+
+    public ReceiptManager(){
+
+        //creating a new folder named receipts if it does not already exist
+        File receiptsFolder = new File("receipts");
+
+        if (!receiptsFolder.exists()) {
+
+            receiptsFolder.mkdirs();
+        }
+    }
+
 }
